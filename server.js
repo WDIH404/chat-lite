a604adb40119e970e39a770318b530f8faa4efdd
WDIH404 <flazy322@gmail.com>
2023-02-01 17:20:26 +0700

    +

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-app.get('/users', function () {
-    console.log('Hello!');
+const rooms = new Map();
+
+app.get('/rooms', (req, res) => {
+    res.json(rooms);
 });
 
+io.on('connection', socket => {
+    console.log('user connected', socket.id);
+});
 
-app.listen(9999);
+server.listen(9999, (err) => {
+    if (err) {
+        throw Error(err);
+    }
+    console.log('Сервер запущен!');
+});
